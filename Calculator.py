e2eef5e5f23410d237698bf90c78313ac93d93a4
Venkateshpenke <Venkatesh.penke6@gmail.com>
2024-08-20 16:45:37 +0530

    Remainder

--- Calculator.py
+++ Calculator.py
@@ -1,3 +1,12 @@
-class Calculator()
+class Calculator:
+      
+    def remainder(self,a,b):
+        c=a%b
+        print(c)
     
-    def 
+
+a=int(input("Enter the Furst Nummber"))
+b=int(input("Enter the Second Number"))
+obj=Calculator()
+obj.remainder(a,b)
+        
