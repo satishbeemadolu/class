da07b1f4cc771fd387f17124690c989c06f7760f
satishbeemadolu <satish.bmdl@gmail.com>
2024-08-21 15:43:31 +0530

    changed code

--- Calculator.py
+++ Calculator.py
@@ -1,15 +1,15 @@
 class Calculator():
     def add(self,a,b):
         c=a+b
-        print(c)
+        print("adition of two numbers",c)
       
     def remainder(self,a,b):
         c=a%b
-        print(c)
+        print("remainder of first number",c)
 
-    def remainder(self,a,b):
-        c=a%b
-        print(c)
+    def multiplication(self,a,b):
+        c=a*b
+        print("multiplication of two numbers",c)
 a=int(input("Enter the first number"))
 b=int(input("Enter the second number"))
 obj=Calculator()
