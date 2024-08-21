259c5404106a2acf0ac1ca14bf71ed36bc069600
satishbeemadolu <satish.bmdl@gmail.com>
2024-08-21 15:16:23 +0530

    modified main

--- Calculator.py
+++ Calculator.py
@@ -1,17 +1,18 @@
 class Calculator():
-     def add(self,a,b):
+    def add(self,a,b):
         c=a+b
         print(c)
-object=Calculator(10,20)
-object.add
-class Calculator:
       
     def remainder(self,a,b):
         c=a%b
         print(c)
-    
 
-a=int(input("Enter the Furst Nummber"))
-b=int(input("Enter the Second Number"))
+    def remainder(self,a,b):
+        c=a%b
+        print(c)
+a=int(input("Enter the first number"))
+b=int(input("Enter the second number"))
 obj=Calculator()
+obj.add(a, b)
+obj.multiplication(a,b)
 obj.remainder(a,b)
